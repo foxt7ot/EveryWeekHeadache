f7ee2f0af9dd048b30575a5817bd09c6592f85b3
Yaseen <myaseen.khan@hotmail.com>
2016-05-04 17:54:32 +0530

    Added some more global variables

--- src/skillnet/everyweek/headache/GlobalResources.java
+++ src/skillnet/everyweek/headache/GlobalResources.java
@@ -5,4 +5,12 @@ package skillnet.everyweek.headache;
  */
 public class GlobalResources {
     public static String directoryStructure;
+    public static final String TIME_ENTRY_DATE = "TimeEntryDate";
+    public static final String PROJECT_NAME = "ProjectName";
+    public static final String TASK_NAME = "TaskName";
+    public static final String DESCRIPTION = "Description";
+    public static final String START_TIME = "StartTime";
+    public static final String END_TIME = "EndTime";
+    public static final String TOTAL_TIME = "TotalTime";
+
 }
